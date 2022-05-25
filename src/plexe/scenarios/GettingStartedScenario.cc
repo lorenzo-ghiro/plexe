36e99a77153e03354b429e3ed1a7024a0bf4be19
Lorenzo <lorenzo.ghiro@unitn.it>
2022-05-25 10:41:14 +0200

    Define GettingStartedScenario, making last vehicle of platoon break and detach from platoon

---
+++ src/plexe/scenarios/GettingStartedScenario.cc
@@ -0,0 +1,102 @@
+//
+// Copyright (C) 2018-2021 Julian Heinovski <julian.heinovski@ccs-labs.org>
+//
+// SPDX-License-Identifier: GPL-2.0-or-later
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+
+#include "plexe/scenarios/GettingStartedScenario.h"
+
+using namespace veins;
+
+namespace plexe {
+
+Define_Module(GettingStartedScenario);
+
+void GettingStartedScenario::initialize(int stage)
+{
+
+    BaseScenario::initialize(stage);
+
+    if (stage == 0)
+        // get pointer to application
+        appl = FindModule<BaseApp*>::findSubModule(getParentModule());
+
+    if (stage == 2) {
+        // average speed
+        leaderSpeed = par("leaderSpeed").doubleValue() / 3.6;
+
+        if (positionHelper->isLeader()) {
+            // set base cruising speed
+            plexeTraciVehicle->setCruiseControlDesiredSpeed(leaderSpeed);
+        }
+        else {
+            // let the follower set a higher desired speed to stay connected
+            // to the leader when it is accelerating
+            plexeTraciVehicle->setCruiseControlDesiredSpeed(leaderSpeed + 10);
+
+            // if we are the last vehicle, then schedule breaking at time 10sec
+            std::vector<int> pFormation = positionHelper->getPlatoonFormation();
+            // if we are the last vehicle...
+            if (positionHelper->getId() == pFormation[pFormation.size() - 1]) {
+                // prepare self messages for scheduled operations
+                startBreaking = new cMessage("Start Breaking now!");
+                checkDistance = new cMessage("Check Distance now!");
+
+                // ...then change Color and schedule Break operation
+                traciVehicle->setColor(TraCIColor(100, 100, 100, 255));
+                scheduleAt(10, startBreaking);
+            }
+        }
+    }
+}
+
+
+void GettingStartedScenario::handleMessage(cMessage* msg)
+{
+    if (msg == startBreaking) {
+        // Increase CACC Constant Spacing (set it to 15m)
+        plexeTraciVehicle->setCACCConstantSpacing(15.0);
+        traciVehicle->setColor(TraCIColor(150, 150, 150, 255));
+        // then start checking when we reach that 15m distance
+        scheduleAt(simTime() + 0.1, checkDistance);
+
+    }
+    else if (msg == checkDistance) {
+        // Checking current distance with radar
+        double distance = nan("nan"), relSpeed = nan("nan");
+        plexeTraciVehicle->getRadarMeasurements(distance, relSpeed);
+        std::cout << "LEAVING VEHICLE now at: " << distance << " meters" << endl;
+
+        if (distance > 14.9) {
+            // We are almost at correct distance! Turn to ACC (i.e., abandon platoon...)
+            plexeTraciVehicle->setACCHeadwayTime(1.2);
+            plexeTraciVehicle->setActiveController(ACC);
+            traciVehicle->setColor(TraCIColor(200, 200, 200, 255));
+        }
+        else {
+            scheduleAt(simTime() + 0.1, checkDistance);
+        }
+    }
+}
+
+GettingStartedScenario::~GettingStartedScenario()
+{
+    cancelAndDelete(startBreaking);
+    cancelAndDelete(checkDistance);
+}
+
+} // namespace plexe
---
+++ src/plexe/scenarios/GettingStartedScenario.h
@@ -0,0 +1,52 @@
+//
+// Copyright (C) 2018-2021 Julian Heinovski <julian.heinovski@ccs-labs.org>
+//
+// SPDX-License-Identifier: GPL-2.0-or-later
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+
+#ifndef SIMPLESCENARIO_H_
+#define SIMPLESCENARIO_H_
+
+#include "plexe/scenarios/BaseScenario.h"
+#include "plexe/apps/BaseApp.h"
+
+namespace plexe {
+
+class GettingStartedScenario : public BaseScenario {
+
+protected:
+    // leader average speed
+    double leaderSpeed;
+    // application layer, used to stop the simulation
+    BaseApp* appl;
+
+private:
+    cMessage* startBreaking;
+    cMessage* checkDistance;
+
+public:
+    virtual void initialize(int stage) override;
+    virtual void handleMessage(cMessage* msg) override;
+    virtual ~GettingStartedScenario();
+    GettingStartedScenario()
+        : leaderSpeed(0)
+        , appl(nullptr){};
+};
+
+} // namespace plexe
+
+#endif
